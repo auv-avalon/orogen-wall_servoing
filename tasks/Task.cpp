652e6283022850350ca680c9dea913447ccd1f5e
Sascha Arnold <sascha.arnold@dfki.de>
2011-06-24 00:15:04 +0200

    added possibility to set ransac parameters for wall estimation

--- tasks/Task.cpp
+++ tasks/Task.cpp
@@ -52,14 +52,16 @@ bool Task::startHook()
     double min_response_value = _min_response_value.get();
     double wall_estimation_start_angle = _wall_estimation_start_angle.get();
     double wall_estimation_end_angle = _wall_estimation_end_angle.get();
+    double ransac_threshold = _wall_estimation_ransac_threshold.get();
+    double ransac_min_inliers = _wall_estimation_ransac_min_inliers.get();
     
-    if (beam_threshold_min < 0 || beam_threshold_max < 0 || beam_threshold_min >= beam_threshold_max)
+    if (beam_threshold_min < 0.0 || beam_threshold_max < 0.0 || beam_threshold_min >= beam_threshold_max)
     {
         std::cerr << "The sonar beam thresholds shouldn't be smaller then 0 and the "
                      << "maximum threshold should be greater than the minimum one." << std::endl;
         return false;
     }
-    if (min_response_value < 0) 
+    if (min_response_value < 0.0) 
     {
         std::cerr << "The minimum response value has to be greater than 0!" << std::endl;
         return false;
@@ -68,6 +70,17 @@ bool Task::startHook()
         wall_estimation_end_angle < M_PI_2 || wall_estimation_end_angle > M_PI + M_PI_2)
     {
         std::cerr << "The wall estimation angles have to be between 1/2 PI and 3/2 PI for this task." << std::endl;
+        return false;
+    }
+    if (ransac_threshold < 0.0)
+    {
+        std::cerr << "The ransac threshold has to be greater than 0 for the wall estimation." << std::endl;
+        return false;
+    }
+    if (ransac_min_inliers > 1.0 || ransac_min_inliers < 0.0)
+    {
+        std::cerr << "The ransac minimum inliers have to be between 0 and 1 for the wall estimation," 
+                     << "because this value is in percent." << std::endl;
         return false;
     }
     
@@ -82,6 +95,7 @@ bool Task::startHook()
     settings.startAngle = wall_estimation_start_angle;
     settings.endAngle = wall_estimation_end_angle;
     wallEstimation->setSettings(settings);
+    wallEstimation->setRansacParameters(ransac_threshold, ransac_min_inliers);
     processing->addSonarEstimation(wallEstimation);
     
     delete distanceEstimation;
