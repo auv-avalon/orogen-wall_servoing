24d9cbf0ceafda5d6da520eaf5c479d488811d8f
Sascha Arnold <sascha.arnold@dfki.de>
2011-09-20 22:07:44 +0200

    changes in the ruby replay script because of the new visualizations

--- scripts/run_log.rb
+++ scripts/run_log.rb
@@ -1,61 +1,78 @@
+require 'orocos'
 require 'vizkit'
+require 'vizkit/widgets/task_inspector/task_inspector'
+include Orocos
 
 Orocos.initialize
 log = Orocos::Log::Replay.open(ARGV)
 
-Orocos.run 'sonardetector', 'sonar_vizkit' do
+Orocos.run 'sonardetector' do
 
     sonardetector = Orocos::TaskContext.get 'sonardetector'
 
     # sonarbeam settings
     sonardetector.enable_beam_threshold = true
-    sonardetector.beam_threshold_min = 1
-    sonardetector.beam_threshold_max = 5
-    sonardetector.min_response_value = 10
+    sonardetector.beam_threshold_min = 1.5
+    sonardetector.beam_threshold_max = 15
+    sonardetector.min_response_value = 30
 
     # wall estimation settings
-    sonardetector.wall_estimation_start_angle = 2.36
-    sonardetector.wall_estimation_end_angle = 3.93
+    sonardetector.wall_estimation_start_angle = -1
+    sonardetector.wall_estimation_end_angle = -1
     sonardetector.wall_estimation_ransac_threshold = 0.5
-    sonardetector.wall_estimation_ransac_min_inliers = 0.7
+    sonardetector.wall_estimation_ransac_min_inliers = 0.6
 
     # controller settings
     sonardetector.wall_distance = 2
-    sonardetector.fixed_depth = 1.5
-    sonardetector.y_distance = 0
-    sonardetector.heading_modulation = 0
-
-    sonar_vizkit = Orocos::TaskContext.get 'sonar_vizkit'
-    # sonarbeam settings
-    sonar_vizkit.enable_beam_threshold = true
-    sonar_vizkit.beam_threshold_min = 1
-    sonar_vizkit.beam_threshold_max = 9
-    sonar_vizkit.min_response_value = 2
-    sonar_vizkit.show_all_beam_entries = false
-    # wall estimation settings
-    sonar_vizkit.enable_wall_estimation = true
-    sonar_vizkit.wall_estimation_start_angle = 2.36
-    sonar_vizkit.wall_estimation_end_angle = 3.93
-    sonar_vizkit.wall_estimation_ransac_threshold = 0.4
-    sonar_vizkit.wall_estimation_ransac_min_inliers = 0.5
-
-    # write debug output
-    sonardetector.debug_output = false
+    sonardetector.fixed_depth = -1.5
+    sonardetector.servoing_speed = 0.2
+    sonardetector.exploration_speed = 0.2
+    sonardetector.heading_modulation = 0.0
 
     log.sonar.BaseScan.connect_to sonardetector.sonar_input
     log.orientation_estimator.orientation_samples.connect_to sonardetector.orientation_sample
-    log.sonar.BaseScan.connect_to sonar_vizkit.sonar_input
-    log.orientation_estimator.orientation_samples.connect_to sonar_vizkit.body_state
+
+    view3d = Vizkit.default_loader.create_widget('vizkit::Vizkit3DWidget')
+    view3d.show()
+    sonarfeatureviz = view3d.createPlugin('sonarfeature', 'SonarFeatureVisualization')
+    wallviz = view3d.createPlugin('wall', 'WallVisualization')
+    auv_avalon = view3d.createPlugin('auv_avalon', 'AUVAvalonVisualization')
+    auv_avalon.showDesiredModelPosition(true)
+    #sonarbeamviz = view3d.createPlugin('sonarbeam', 'SonarBeamVisualization')
+
+    # Connect debug port to vizkit plugins
+    con = Vizkit.connect_port_to 'sonardetector', 'wall_data', :type => :buffer, :size => 100, :auto_reconnect => true, :pull => false, :update_frequency => 33 do |sample, name|
+        sonarfeatureviz.updatePointCloud(sample.pointCloud)
+        wallviz.updateWallData(sample.wall)
+        sample
+    end 
+
+    con = Vizkit.connect_port_to 'sonardetector', 'position_command', :type => :buffer, :size => 100, :auto_reconnect => true, :pull => false, :update_frequency => 33 do |sample, name|
+        auv_avalon.updateDesiredPosition(sample)
+        sample
+    end 
+
+    log.orientation_estimator.orientation_samples :type => :buffer, :size => 100  do |sample|
+        auv_avalon.updateRigidBodyState(sample)
+        #sonarbeamviz.updateBodyState(sample)
+        sample
+    end 
+    
+    #log.sonar.BaseScan :type => :buffer, :size => 100  do |sample|
+    #    sonarbeamviz.updateSonarScan(sample)
+    #    sample
+    #end 
 
     sonardetector.configure
     sonardetector.start
-    sonar_vizkit.configure
-    sonar_vizkit.start
     viewer = Vizkit.default_loader.StructViewer
     log.sonar.BaseScan.connect_to viewer
     viewer.show
 
     Vizkit.display sonardetector.position_command
+    task_inspector = Vizkit.default_loader.task_inspector
+    task_inspector.config(sonardetector)
+    task_inspector.show
     Vizkit.control log
     Vizkit.exec
 end
