c7966c9deafc429c85415342ba04b26076b426f7
Sascha Arnold <sascha.arnold@dfki.de>
2012-02-15 22:30:20 +0100

    move and update log replay script

--- scripts/replay/run_log.rb
+++ scripts/replay/run_log.rb
@@ -5,32 +5,34 @@ include Orocos
 
 Orocos.initialize
 log = Orocos::Log::Replay.open(ARGV)
+sonar_port = log.find_port("/base/samples/SonarBeam")
+puts sonar_port.stream.info.interval_rt.first
 
-Orocos.run 'wall_servoing' do
+Orocos.run 'wall_servoing', 'sonar_feature_estimator' do
 
     sonardetector = Orocos::TaskContext.get 'wall_servoing'
-
-    # sonarbeam settings
-    sonardetector.enable_beam_threshold = true
-    sonardetector.beam_threshold_min = 1.5
-    sonardetector.beam_threshold_max = 15
-    sonardetector.min_response_value = 30
+    feature_estimator = Orocos::TaskContext.get 'sonar_feature_estimator'
 
     # wall estimation settings
-    sonardetector.wall_estimation_start_angle = -1
-    sonardetector.wall_estimation_end_angle = -1
-    sonardetector.wall_estimation_ransac_threshold = 0.5
-    sonardetector.wall_estimation_ransac_min_inliers = 0.6
+    sonardetector.wall_estimation_start_angle = Math::PI * 0.25
+    sonardetector.wall_estimation_end_angle = -Math::PI * 0.25
+    sonardetector.wall_estimation_ransac_threshold = 0.2
+    sonardetector.wall_estimation_ransac_min_inliers = 0.8
+    sonardetector.dbscan_epsilon = 0.08726646259971647 * 1.5
+    sonardetector.fading_out_factor = 0.02
+    sonardetector.enable_debug_output = true
 
     # controller settings
-    sonardetector.wall_distance = 2
+    sonardetector.wall_distance = 3
     sonardetector.fixed_depth = -1.5
-    sonardetector.servoing_speed = 0.2
-    sonardetector.exploration_speed = 0.2
-    sonardetector.heading_modulation = 0.0
+    sonardetector.servoing_speed = 0.5
+    sonardetector.exploration_speed = 0.1
+    sonardetector.heading_modulation = 0.41
 
-    log.sonar.BaseScan.connect_to sonardetector.sonar_input
+    sonar_port.connect_to feature_estimator.sonar_input
+    feature_estimator.new_feature.connect_to sonardetector.sonarbeam_feature
     log.orientation_estimator.orientation_samples.connect_to sonardetector.orientation_sample
+    log.orientation_estimator.orientation_samples.connect_to feature_estimator.orientation_sample
 
     view3d = Vizkit.default_loader.create_widget('vizkit::Vizkit3DWidget')
     view3d.show()
@@ -41,7 +43,7 @@ Orocos.run 'wall_servoing' do
     #sonarbeamviz = view3d.createPlugin('sonarbeam', 'SonarBeamVisualization')
 
     # Connect debug port to vizkit plugins
-    con = Vizkit.connect_port_to 'wall_servoing', 'wall_data', :type => :buffer, :size => 100, :auto_reconnect => true, :pull => false, :update_frequency => 33 do |sample, name|
+    con = Vizkit.connect_port_to 'wall_servoing', 'wall_servoing_debug', :type => :buffer, :size => 100, :auto_reconnect => true, :pull => false, :update_frequency => 33 do |sample, name|
         sonarfeatureviz.updatePointCloud(sample.pointCloud)
         wallviz.updateWallData(sample.wall)
         sample
@@ -65,6 +67,9 @@ Orocos.run 'wall_servoing' do
 
     sonardetector.configure
     sonardetector.start
+
+    feature_estimator.start
+
     viewer = Vizkit.default_loader.StructViewer
     log.sonar.BaseScan.connect_to viewer
     viewer.show
