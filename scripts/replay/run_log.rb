01efe6502c6043bbc3526c27744e187f3eadc793
Sascha Arnold <sascha.arnold@dfki.de>
2012-07-12 14:18:44 +0200

    added some missing properties and changes

--- scripts/replay/run_log.rb
+++ scripts/replay/run_log.rb
@@ -5,8 +5,8 @@ include Orocos
 Orocos.initialize
 log = Orocos::Log::Replay.open(ARGV, Typelib::Registry.new)
 #sonar_port = log.find_port("/base/samples/SonarBeam")
-sonar_port = log.sonar.BaseScan
-orentation_port = log.state_estimator.orientation_samples
+sonar_port = log.sonar.sonar_beam
+orentation_port = log.depth_orientation_fusion.pose_samples
 puts sonar_port.stream.info.interval_rt.first
 
 Orocos.run 'wall_servoing_test', 'sonar_feature_estimator_test' do
@@ -14,23 +14,31 @@ Orocos.run 'wall_servoing_test', 'sonar_feature_estimator_test' do
     sonardetector = Orocos::TaskContext.get 'wall_servoing'
     feature_estimator = Orocos::TaskContext.get 'sonar_feature_estimator'
     #feature_estimator.derivative_history_length = 1
+    feature_estimator.enforce_line_rate = 0.5
+    feature_estimator.enforce_line_beam_covariance = 0.2
+    feature_estimator.minimum_enforce_line_value = 1.0
+    feature_estimator.avalon_boundary_box_size = 1.2
+    feature_estimator.proportional_value_threshold = 0.5
+    feature_estimator.signal_threshold = 1.5
+    feature_estimator.feature_threshold = 0.7
+    feature_estimator.signal_balancing = 0.2
 
     # wall estimation settings
-    sonardetector.left_opening_angle = Math::PI * 0.35
-    sonardetector.right_opening_angle = Math::PI * 0.25
+    sonardetector.left_opening_angle = Math::PI * 0.5
+    sonardetector.right_opening_angle = Math::PI * 0.35
     sonardetector.wall_estimation_ransac_threshold = 0.2
     sonardetector.wall_estimation_ransac_min_inliers = 0.8
     sonardetector.dbscan_epsilon = 0.08726646259971647 * 1.5
-    sonardetector.fading_out_factor = 0.02
+    sonardetector.fading_out_factor = 0.01
     sonardetector.enable_debug_output = true
 
     # controller settings
-    sonardetector.wall_distance = 3
-    sonardetector.fixed_depth = -1.5
-    sonardetector.servoing_speed = 0.5
+    sonardetector.wall_distance = 3.5
+    sonardetector.fixed_depth = -1.1
+    sonardetector.servoing_speed = 0.4
     sonardetector.exploration_speed = 0.1
-    sonardetector.servoing_wall_direction = -0.4
-    sonardetector.inital_wall_direction = 0.0
+    sonardetector.servoing_wall_direction = -Math::PI*0.5 # -0.4
+    sonardetector.initial_wall_direction = -Math::PI*0.5 # 0.0
 
     sonar_port.connect_to feature_estimator.sonar_input, :type => :buffer, :size => 100
     feature_estimator.new_feature.connect_to sonardetector.sonarbeam_feature, :type => :buffer, :size => 100
@@ -43,7 +51,7 @@ Orocos.run 'wall_servoing_test', 'sonar_feature_estimator_test' do
     wallviz = Vizkit.default_loader.WallVisualization
     auv_avalon = Vizkit.default_loader.AUVAvalonVisualization
     auv_avalon.showDesiredModelPosition(true)
-    #sonarbeamviz = Vizkit.default_loader.SonarBeamVisualization
+    sonarbeamviz = Vizkit.default_loader.SonarBeamVisualization
 
     # Connect debug port to vizkit plugins
     con = Vizkit.connect_port_to 'wall_servoing', 'wall_servoing_debug', :pull => false, :update_frequency => 33 do |sample, name|
@@ -55,13 +63,13 @@ Orocos.run 'wall_servoing_test', 'sonar_feature_estimator_test' do
         auv_avalon.updateDesiredPosition(sample)
     end 
 
-    log.state_estimator.orientation_samples do |sample|
+    log.depth_orientation_fusion.pose_samples do |sample|
         auv_avalon.updateRigidBodyState(sample)
-        #sonarbeamviz.updateBodyState(sample)
+        #sonarbeamviz.updateOrientation(sample)
         sample
     end 
     
-    #log.sonar.BaseScan do |sample|
+    #log.sonar.sonar_beam do |sample|
     #    sonarbeamviz.updateSonarBeam(sample)
     #    sample
     #end 
@@ -70,9 +78,8 @@ Orocos.run 'wall_servoing_test', 'sonar_feature_estimator_test' do
 
     feature_estimator.start
 
-    viewer = Vizkit.default_loader.StructViewer
-    log.sonar.BaseScan.connect_to viewer
-    viewer.show
+    Vizkit.display log.sonar
+    Vizkit.display feature_estimator
 
     task_inspector = Vizkit.default_loader.task_inspector
     task_inspector.config(sonardetector)
